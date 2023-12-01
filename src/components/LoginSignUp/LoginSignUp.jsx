8f426c75d64ec93769c741c43a70823c6586e79c
sujit1266 <122362384+sujit1266@users.noreply.github.com>
2023-12-02 00:30:58 +0530

    Added New Details on SignUp Page and Disable the Icons

--- src/components/LoginSignUp/LoginSignUp.jsx
+++ src/components/LoginSignUp/LoginSignUp.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./LoginSignUp.css";
-import user_icon from "../Assets/person.png";
-import email_icon from "../Assets/email.png";
-import password_icon from "../Assets/password.png";
+// import user_icon from "../Assets/person.png";
+// import email_icon from "../Assets/email.png";
+// import password_icon from "../Assets/password.png";
 
 const LoginSignUp = () => {
   const [action, setAction] = useState("Sign Up");
@@ -17,23 +17,37 @@ const LoginSignUp = () => {
           {action === "Login" ? (
             <div></div>
           ) : (
+            <>
             <div className="input">
-              <img src={user_icon} alt="User Icon" />
+              {/* <img src={user_icon} alt="User Icon" /> */}
               <div className="inputinner-div">
                 <input type="text" placeholder="Name" />
               </div>
             </div>
+            <div className="input">
+              {/* <img src={user_icon} alt="User Icon" /> */}
+              <div className="inputinner-div">
+                <input type="text" placeholder="Address" />
+              </div>
+            </div>
+            <div className="input">
+              {/* <img src={user_icon} alt="User Icon" /> */}
+              <div className="inputinner-div">
+                <input type="text" placeholder="Phone Number" />
+              </div>
+            </div>
+            </>
           )}
 
           {/* <img src={email_icon} alt="User Email" /> */}
           <div className="input">
-            <img src={email_icon} alt="User Email" />
+            {/* <img src={email_icon} alt="User Email" /> */}
             <div className="inputinner-div">
               <input type="email" placeholder="Email" />
             </div>
           </div>
           <div className="input">
-            <img src={password_icon} alt="User Password" />
+            {/* <img src={password_icon} alt="User Password" /> */}
             <div className="inputinner-div">
               <input type="password" placeholder="Password" />
             </div>
