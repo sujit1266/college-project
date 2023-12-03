3829375f596a24d3526e683ac89d6229c8eaf0cb
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2023-12-03 13:50:54 +0530

    Updated Css File Path Added

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./LoginSignUp.css";
+import "../styling/LoginSignUp.css";
 import { Link } from "react-router-dom";
 // import user_icon from "../Assets/person.png";
 // import email_icon from "../Assets/email.png";
