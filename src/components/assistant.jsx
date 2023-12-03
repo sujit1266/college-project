7025b18ca79baaf5e8e770a837ad2c44285cd74c
Debmalya Biswas <viperbale.db@gmail.com>
2023-12-03 23:07:25 +0530

    api update

--- src/components/assistant.jsx
+++ src/components/assistant.jsx
@@ -7,47 +7,97 @@ import { BsMoisture } from "react-icons/bs";
 import { FaCloudShowersHeavy } from "react-icons/fa";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useEffect, useState } from "react";
+import { RiCelsiusFill } from "react-icons/ri";
+import axios from "axios";
 
-
+const API_endpoint = `https://api.openweathermap.org/data/2.5`;
+const API_key = `7fcc9cf023a47757aa5263e37dac405f`;
 const Assistant = () => {
-     const position = { lat: 53.54, lng: 10 };
+  const position = { lat: 53.54, lng: 10 };
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [response, setResponse] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const position = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
 
-     return (
-          <div className="assistant">
-               <div className="div1">
-                    <div>
-                         <h1>Your Agriculture Assistant</h1>
-                         <p>Helping You To Make The Right Decision and Seed Your Plant</p>
-                    </div>
-                    <div className="options">
-                         <IoIosSearch className="search-icon icons" />
-                         <IoMdNotificationsOutline className="icons" />
-                         <div className="profile">
-                              <div className="inner-profile"></div>
-                         </div>
-                    </div>
-               </div>
-               <div className="showWeather">
-                    <Eachreport icon={<CiTempHigh className="report-icons" />} measurment="+20℃" name="Air Temp"  type="Good" classname="air temp"/>
-                    <Eachreport icon={<BsMoisture className="report-icons" />} measurment="72%" name="Soil Moisture"  type="High" classname="soil temp"/>
-                    <Eachreport icon={<FaCloudShowersHeavy className="report-icons" />} measurment="-2mm" name="preciptiation" type="Low"  classname="pre temp"/>
-               </div>
-               <div className="field-text">
-                    <h2>Manage Your fields</h2>
-                    <p>Select an area for showing</p>
-                    <button><FaMapMarkerAlt className="map-icon"/>Ready To Show</button>
-               </div>
-                
-               <div className="field">
-        <div className="App" style={{ height: "43vh", width: "100%",   }}>
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+
+        const finalAPIEndPoint = `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=fff910b5c1094a88a1cf204e71032260&include=minutely`;
+        const response = await axios.get(finalAPIEndPoint);
+        console.log(response?.data?.data[0]);
+        setResponse(response?.data?.data[0]);
+        console.log(finalAPIEndPoint);
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="assistant">
+      <div className="div1">
+        <div>
+          <h1>Your Agriculture Assistant</h1>
+          <p>Helping You To Make The Right Decision and Seed Your Plant</p>
+        </div>
+        <div className="options">
+          <IoIosSearch className="search-icon icons" />
+          <IoMdNotificationsOutline className="icons" />
+          <div className="profile">
+            <div className="inner-profile"></div>
+          </div>
+        </div>
+      </div>
+      <div className="showWeather">
+        <Eachreport
+          icon={<CiTempHigh className="report-icons" />}
+          measurment={`${response?.temp} °C ${response?.app_temp} °C`}
+          name="Temp"
+          type="Good"
+          classname="air temp"
+        />
+        <Eachreport
+          icon={<BsMoisture className="report-icons" />}
+          measurment="72%"
+          name="Soil Moisture"
+          type="High"
+          classname="soil temp"
+        />
+        <Eachreport
+          icon={<FaCloudShowersHeavy className="report-icons" />}
+          measurment={`${response?.precip} mm`}
+          name={response?.weather?.description}
+          type="Low"
+          classname="pre temp"
+        />
+      </div>
+      <div className="field-text">
+        <h2>Manage Your fields</h2>
+        <p>Select an area for showing</p>
+        <button>
+          <FaMapMarkerAlt className="map-icon" />
+          Ready To Show
+        </button>
+      </div>
+
+      <div className="field">
+        <div className="App" style={{ height: "43vh", width: "100%" }}>
           <APIProvider apiKey={"AIzaSyBYVCPwJEVI2t7HQFbobyR_c-Eqay7N9zI "}>
             <Map zoom={9} center={position} mapId={"35817c4a428adc97"}></Map>
           </APIProvider>
         </div>
       </div>
-                
-          </div>
-     )
-}
+    </div>
+  );
+};
 
 export default Assistant;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-     <App/>,
-     document.getElementById('root')
-)
+const root = document.getElementById("root");
+
+// Use createRoot to render the App component
+const rootInstance = ReactDOM.createRoot(root);
+rootInstance.render(<App />);
