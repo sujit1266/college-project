790bab295b6ba763a92820441c6b36d9775a30f6
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2023-12-03 14:01:39 +0530

    Login Heading Updated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
     <div className="container">
       <div className="inner-container">
         <div className="header">
-          <div className="text">Sign Up</div>
+          <div className="text">Login</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
