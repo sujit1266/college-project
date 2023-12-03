bc94fe727e227fbcf0c2c3baa82086f1101da5b8
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2023-12-03 13:49:54 +0530

    Added Login Route

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Main";
 import LoginSignUp from "./Pages/LoginSignUp";
-
-
+import Login from "./Pages/Login";
 
 const App = () => {
-     return (
-          <Router>
-               <div className="home">
-                    <Routes>
-                         <Route path="/" element={<Main />} />
-                         <Route path="/login" element={<LoginSignUp/>}/>
-                    </Routes>
-               </div>
-          </Router>
-     )
-}
-
+  return (
+    <Router>
+      <div className="home">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<LoginSignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
